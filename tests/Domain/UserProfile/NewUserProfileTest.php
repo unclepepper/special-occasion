25fc489c1df246d34715fcad6d6657c9f8a61509
Nikolay Terleev <unclepepper.dev@gmail.com>
2025-03-29 19:30:30 +0300

    feat: Move to test

--- tests/Domain/UserProfile/NewUserProfileTest.php
+++ tests/Domain/UserProfile/NewUserProfileTest.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace App\Tests\Domain\UserProfile;
 
 use App\Domain\UserProfile\Dto\UserProfileDto;
+use App\Domain\UserProfile\Dto\UserProfileInfoDto;
 use App\Domain\UserProfile\Entity\Event\UserProfileEvent;
 use App\Domain\UserProfile\Entity\UserProfile;
 use App\Domain\UserProfile\Enum\UserGenderEnum;
+use App\Domain\UserProfile\Enum\UserStatusEnum;
 use App\Domain\UserProfile\UseCase\New\UserProfileHandler;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
@@ -34,15 +36,28 @@ class NewUserProfileTest extends KernelTestCase
         /** @var UserProfileHandler $handler */
         $handler = self::getContainer()->get(UserProfileHandler::class);
 
+        $userProfileInfo          = new UserProfileInfoDto();
+        $userProfileInfo->name    = 'name-test';
+        $userProfileInfo->surname = 'surname-test';
+        $userProfileInfo->status  = UserStatusEnum::ACTIVE;
+
+
         /** Create Dto for UserProfile */
         $userProfileDto           = new UserProfileDto();
         $userProfileDto->username = 'TestUsername';
         $userProfileDto->birthday = new DateTimeImmutable('1981-03-07 08:30:00');
         $userProfileDto->gender   = UserGenderEnum::MALE;
+        $userProfileDto->info = $userProfileInfo;
 
         self::assertEquals('TestUsername', $userProfileDto->username);
         self::assertEquals('1981-03-07 08:30:00', $userProfileDto->birthday->format('Y-m-d H:i:s'));
         self::assertEquals(UserGenderEnum::MALE, $userProfileDto->gender);
+
+
+        self:: assertEquals('name-test', $userProfileDto->info->name);
+        self:: assertEquals('surname-test', $userProfileDto->info->surname);
+        self:: assertEquals( UserStatusEnum::ACTIVE, $userProfileDto->info->status);
+        self:: assertEquals($userProfileInfo, $userProfileDto->info);
 
         /** Create UserProfile and UserProfileEvent via Dto */
         $UserProfile = $handler->handle($userProfileDto);
